3523c7060e5bd96f4f64d4eb7ae04e534c328211
R. Allan White <allan@catalyze.io>
2019-04-19 14:02:46 -0700

    Hiding default home content, basic info and link to docs.

---
+++ src/components/home/HomeBasic.vue
@@ -0,0 +1,42 @@
+<template>
+  <Section dots="true">
+    <div class="homepage-features container post">
+      <div class="grid-cols grid-cols--1 mb">
+        <div style="order:2">
+          <h3>Documentation Demo</h3>
+          <p>We have a pile of markdown files; let's make them look decent, load fast, and be easy to search and read.</p>
+          <p>This demo is a fork of <a href="https://github.com/gridsome/gridsome">Gridsome.org</a>.</p>
+            <p class="home-links">
+            <g-link  to="/docs" class="button primary">
+                See the docs
+            </g-link>
+            <!-- <g-link  to="/learn" class="button">
+                Tutorial
+            </g-link> -->
+            </p>
+          </div>
+      </div>
+    </div>
+  </Section>
+</template>
+
+<script>
+import Bullet from '~/assets/images/bullet.svg'
+// import VueLogo from '~/assets/images/vue-logo.svg'
+
+export default {
+  components: {
+    Bullet
+  },
+}
+
+</script>
+
+
+<static-query>
+query Example {
+  example (path: "/examples/templates") {
+    content
+  }
+}
+</static-query>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,38 +1,40 @@
 <template>
   <Layout>
-    <home-intro />
+    <home-basic />
+    <!-- <home-intro />
     <home-features />
     <home-fast />
     <home-how-it-works />
     <home-tabs />
-    <home-blog />
+    <home-blog /> -->
   </Layout>
 </template>
 
 <script>
+import HomeBasic from '@/components/home/HomeBasic.vue'
+// import HomeIntro from '@/components/home/HomeIntro.vue'
+// import HomeFeatures from '@/components/home/HomeFeatures.vue'
+// import HomeTabs from '@/components/home/HomeTabs.vue'
+// import HomeFast from '@/components/home/HomeFast.vue'
 
-import HomeIntro from '@/components/home/HomeIntro.vue'
-import HomeFeatures from '@/components/home/HomeFeatures.vue'
-import HomeTabs from '@/components/home/HomeTabs.vue'
-import HomeFast from '@/components/home/HomeFast.vue'
-
-import HomeBlog from '@/components/home/HomeBlog.vue'
-import HomeHowItWorks from '@/components/home/HomeHowItWorks.vue'
+// import HomeBlog from '@/components/home/HomeBlog.vue'
+// import HomeHowItWorks from '@/components/home/HomeHowItWorks.vue'
 
 export default {
   components: {
-    HomeIntro,
-    HomeFeatures,
-    HomeTabs,
-    HomeFast,
-    HomeBlog,
-    HomeHowItWorks,
+    HomeBasic,
+    // HomeIntro,
+    // HomeFeatures,
+    // HomeTabs,
+    // HomeFast,
+    // HomeBlog,
+    // HomeHowItWorks,
   },
   
   metaInfo: {
-    title: 'Vue.js-powered static site generator',
+    title: 'Basic Documentation Stack',
     meta: [
-      { name: 'description', content: 'Gridsome is an open-source, Vue-powered modern site generator that builds performance into every website automatically. Gridsome makes it easy and fun for developers to create fast, beautiful websites without needing to become a performance expert.'
+      { name: 'description', content: 'This is a demo for a documenation stack solution; built with Gridsome (gridsome.org)'
       }
     ],
   }
